1b1564706bc1dd1fbcca6cc0f7ee45ec857fbad2
Maurício <mauro.cavalcante18@gmail.com>
2024-06-25 19:19:28 -0300

    variaveis e constantes

---
+++ src/Variaveis.java
@@ -0,0 +1,21 @@
+public class Variaveis {
+
+    static final float VALOR_PI = 3.14f;
+
+    public static void main(String[] args) {
+        // variável é um valor volátil
+
+        int idade;
+        idade = 25;
+
+        // constante PI -> 3,14
+
+        System.out.println("A minha idade é: " + idade);
+
+        System.out.println("Valor do PI é: " + VALOR_PI);
+
+
+
+
+    }
+}
