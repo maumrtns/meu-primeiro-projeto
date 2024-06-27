bbb3c06fab8e772a27c0172a06fb0f2241290666
Maurício <mauro.cavalcante18@gmail.com>
2024-06-26 21:11:20 -0300

    introducao tipos primitivos
    Signed-off-by: Maurício <mauro.cavalcante18@gmail.com>

---
+++ src/ExemploVariaveis.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+
+public class ExemploVariaveis {
+    public static void main(String[] args) {
+        int idade = 27;
+        double altura = 1.87;
+        //Somente um caractere (char) com aspas simples ''.
+        char genero = 'M';
+        boolean ativo = true;
+        String nome = "Maurício";
+
+        String cpf = "01588785668";
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Ativo? " + ativo);
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+
+
+    }
+}
