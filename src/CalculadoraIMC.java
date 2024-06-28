bb2e21e617cfc3b1b289ff411dc36be6f6389f24
Maurício Martins <mauriciocamartins@gmail.com>
2024-06-27 21:16:27 -0300

    Lendo dados no teclado
    Signed-off-by: Maurício Martins <mauriciocamartins@gmail.com>

--- src/CalculadoraIMC.java
+++ src/CalculadoraIMC.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 public class CalculadoraIMC {
     public static void main(String[] args) {
+
+        Scanner leitorTeclado = new Scanner(System.in);
+
         // peso div (altura * altura)
 
         //pode também ser Float igual a aula
-        double peso = 79.9;
-        double altura = 1.87;
+        System.out.println("Digite o seu peso (em kilos): ");
+        float peso = leitorTeclado.nextFloat();
+        System.out.println("Digite sua altura (em centimetros):");
+        float altura = leitorTeclado.nextFloat();
 
-        double imc = peso / (altura * altura);
-        //Dica para não deixar o double com números infinitos
-        String imcFormatado = String.format("%.2f", imc);
+        float resultado = peso / (altura * altura);
 
-        System.out.println("Seu IMC é de: " + imcFormatado);
+        System.out.println(
+                "O IMC para o peso " + peso + " , e altura de "
+                        + altura + " é " + resultado);
+
+        leitorTeclado.close();
     }
 }
--- src/CalculadoraIdade.java
+++ src/CalculadoraIdade.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
+
 public class CalculadoraIdade {
     public static void main(String[] args) {
         //1 -> pegar o ano atual
         //2 -> pegar o ano de nascimento
         //3 -> subtrair o ano atual - ano de nascimento
 
-        int anoAtual = 2024;
-        int anoNascimento = 1996;
+        Scanner leitorTeclado = new Scanner(System.in);
+
+        System.out.println("Digite o ano atual: ");
+        int anoAtual = leitorTeclado.nextInt();
+        System.out.println("Digite o ano de seu nascimento: ");
+        int anoNascimento = leitorTeclado.nextInt();
 
         int resultado = anoAtual - anoNascimento;
 
         System.out.println("A idade é de: " + resultado);
+
+        leitorTeclado.close();
     }
 }
