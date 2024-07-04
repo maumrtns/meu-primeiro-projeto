1a029df3be7ef4d628cd538afbbc4f1c26b74c95
Maurício Martins <mauriciocamartins@gmail.com>
2024-07-03 22:23:10 -0300

    Loop com DOWHILE
    Signed-off-by: Maurício Martins <mauriciocamartins@gmail.com>

---
+++ src/LoopComDoWhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LoopComDoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+
+        do {
+            System.out.println("Digite um número entre 1 e 10:");
+            numero = scanner.nextInt();
+        } while (numero < 1 || numero > 10);
+
+        System.out.println("O número digitado e valido foi: " + numero);
+        scanner.close();
+    }
+}
