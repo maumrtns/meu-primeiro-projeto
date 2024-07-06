46a4b6452ede540e2e7d42303b8f804b63ec74be
Maurício Martins <mauriciocamartins@gmail.com>
2024-07-05 21:02:36 -0300

    Escopo e valores padrão de variáveis
    Signed-off-by: Maurício Martins <mauriciocamartins@gmail.com>

---
+++ src/ValoresPadrao.java
@@ -0,0 +1,19 @@
+public class ValoresPadrao {
+    static int numero;
+    static float numeroFloat;
+    static boolean verdadeiroOuFalso;
+    static String nome;
+    public static void main(String[] args) {
+        System.out.println(numero);
+        System.out.println(numeroFloat);
+        System.out.println(verdadeiroOuFalso);
+        System.out.println(nome);
+
+        if (verdadeiroOuFalso){
+            int outroNumero = 1;
+            System.out.println(outroNumero);
+        }
+
+
+    }
+}
