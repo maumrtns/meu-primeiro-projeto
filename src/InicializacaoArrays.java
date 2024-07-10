f769842bd0cece086001e623abf5c97130cc756a
Maurício Martins <mauriciocamartins@gmail.com>
2024-07-09 22:19:56 -0300

    Declarando elementos e acessando elementos em um Array
    Signed-off-by: Maurício Martins <mauriciocamartins@gmail.com>

---
+++ src/InicializacaoArrays.java
@@ -0,0 +1,18 @@
+public class InicializacaoArrays {
+    public static void main(String[] args) {
+
+        String nome = "Fulano da silva";
+        String telefone1 = "1125854878";
+        String telefone2 = "2186978545";
+        String[] filhos = {"Zé", "Maria", "Pedro", "Rita"};
+
+        int idade = 30;
+        int[] numeroImpares = {1,3,5,7,9};
+
+        System.out.println(filhos[0]);
+        System.out.println(filhos[1]);
+        System.out.println(filhos[2]);
+        System.out.println(filhos[3]);
+
+    }
+}
